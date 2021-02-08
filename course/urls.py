de1aafa87e12dacaf323dec5eabb8d976e2aa8c8
nareshme302 <nareshme302@gmail.com>
2021-02-08 11:08:54 +0530

    new

--- course/urls.py
+++ course/urls.py
@@ -6,6 +6,8 @@ urlpatterns=[
     path("course/",views.home,name="home"),
     path("course/<int:course_id>/",views.course_details,name="course_details"),
     path("course/<int:course_id>/<int:lesson_id>/", views.lesson_details, name="lesson_details"),
-    path("membership/<int:m_id>/",views.course_membership,name="course_membership"),
+    path("membership/",views.course_membership,name="course_membership"),
+    path("signin/",views.user_signup),
+    path("login/",views.user_login),
+    path("logout/", views.user_logout),
 ]
-
--- course/views.py
+++ course/views.py
@@ -1,14 +1,21 @@
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.shortcuts import HttpResponseRedirect, get_object_or_404, get_list_or_404
 from .models import Lesson, Course, Subscription, UserMembership, Membership
+from django.contrib.auth.models import User
 
 
 # Create your views here.
+@login_required(login_url='/login/')
 def home(request):
     courses = get_list_or_404(Course)
     return render(request, "course.html", {"courses": courses})
 
 
+@login_required(login_url='/login/')
 def course_details(request, course_id):
     # breakpoint()
     course = Course.objects.get(id=course_id)
@@ -17,13 +24,107 @@ def course_details(request, course_id):
     return render(request, "course_details.html", {"course": course, "lessons": lessons, "type": m_type})
 
 
+@login_required(login_url='/login/')
 def lesson_details(request, course_id, lesson_id):
     course = Course.objects.get(id=course_id)
     lesson = course.lesson_set.get(id=lesson_id)
     return render(request, "lesson_details.html", {"lesson": lesson})
 
 
-def course_membership(request,m_id):
-    memberships = Membership.objects.get(id=m_id)
-    membership_courses= memberships.course_set.all()
-    return render(request,"course_membership.html",{"memberships":memberships,"membership_courses":membership_courses})
+@login_required(login_url='/login/')
+def course_membership(request):
+    member_ships = Membership.objects.all()
+    return render(request, "course_membership.html", {"memberships": member_ships})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def user_signup(request):
+    if request.user.is_authenticated:
+        return HttpResponseRedirect('/course/')
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/login/')
+        else:
+            return render(request, 'signin.html', {'form': form})
+    else:
+        form = UserCreationForm()
+        return render(request, 'signin.html', {'form': form})
+
+
+def user_login(request):
+    if request.user.is_authenticated:
+        return HttpResponseRedirect("/course/")
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect('/course/')
+        else:
+            form = AuthenticationForm(request.POST)
+            return render(request, 'login.html', {'form': form})
+    else:
+        form = AuthenticationForm()
+        return render(request, 'login.html', {'form': form})
+
+
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect('/login/')
